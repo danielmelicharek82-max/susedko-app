be906bb36e02e78b0889d53379b560b3fcf6b6ae
Daniel Melicharek <tvoj@email.com>
2026-05-10 22:30:27 +0200

    Fix: add SceneDelegate to fix black screen on iOS

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -1,6 +1,16 @@
-import Flutter
 import UIKit
+import Flutter
 
-class SceneDelegate: FlutterSceneDelegate {
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
 
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: windowScene)
+        let flutterVC = FlutterViewController()
+        window?.rootViewController = flutterVC
+        window?.makeKeyAndVisible()
+    }
 }
